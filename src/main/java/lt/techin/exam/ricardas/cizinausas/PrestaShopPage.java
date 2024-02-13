53c5e9a803c33990bcf1f960c5f9834fe399e6e7
Ricardas007 <rychmaister@gmail.com>
2024-02-13 14:10:12 +0200

    Product to wishlist and add to cart test was made

--- src/main/java/lt/techin/exam/ricardas/cizinausas/PrestaShopPage.java
+++ src/main/java/lt/techin/exam/ricardas/cizinausas/PrestaShopPage.java
@@ -1,8 +1,12 @@
 package lt.techin.exam.ricardas.cizinausas;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+
+import java.time.Duration;
 
 public class PrestaShopPage extends BasePage {
     public PrestaShopPage(WebDriver driver) {
@@ -10,10 +14,83 @@ public class PrestaShopPage extends BasePage {
 
     }
     @FindBy(css = ".products.row > div:nth-of-type(1) > .js-product-miniature.product-miniature h3 > a")
-    WebElement getProductName;
+    WebElement getProductFirstName;
 
-    public String getProductName(){
-        String nameText = getProductName.getText();
+    @FindBy(css = ".products.row > div:nth-of-type(2) > .js-product-miniature.product-miniature h3 > a")
+    WebElement getGetProductSecondName;
+
+    @FindBy(css = ".products.row [alt='Hummingbird printed t-shirt']")
+    WebElement firstProductPicture;
+
+    @FindBy(css = ".wishlist-button-product .material-icons")
+    WebElement firstItemAddToWishlist;
+
+    @FindBy(css = ".modal-text")
+    WebElement firstItemWarningMessageCatch;
+
+    @FindBy(css = ".products.row [alt='Brown bear printed sweater']")
+    WebElement secondProductPicture;
+
+    @FindBy(css = ".wishlist-button-product .material-icons")
+    WebElement secondItemAddToWishlist;
+
+    @FindBy(css = ".modal-text")
+    WebElement secondItemWarningMessageCatch;
+
+    @FindBy(css = ".show .btn-secondary")
+    WebElement firstItemClickCansel;
+
+    @FindBy(css = "select#group_1")
+    WebElement sizeDropDown;
+
+    @FindBy(css = ".add-to-cart.btn.btn-primary")
+    WebElement addToCart;
+
+    @FindBy(css = "h4")
+    WebElement addToCartProductInShoppingCart;
+
+
+    public String getProductNameOne(){
+        String nameText = getProductFirstName.getText();
         return nameText;
+    }
+
+    public String getProductNameTwo(){
+        String nameText = getGetProductSecondName.getText();
+        return nameText;
+    }
+
+    public String firstItemTowhishlist(){
+        firstProductPicture.click();
+        firstItemAddToWishlist.click();
+        String name = firstItemWarningMessageCatch.getText();
+        return name;
+    }
+
+    public String secondItemToWhislist(){
+        secondProductPicture.click();
+        secondItemAddToWishlist.click();
+        String name = secondItemWarningMessageCatch.getText();
+        return name;
+    }
+
+
+    public void firstItemClickCansel() {
+        firstItemClickCansel.click();
+    }
+
+    public void selectItemSize() {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].value='4'", sizeDropDown);
+        addToCart.click();
+    }
+    public String selectProductItemMessageAddedToCart(){
+//        Wait<WebDriver> wait1 = new FluentWait<>(driver)
+//                .withTimeout(Duration.ofSeconds(5))
+//                .pollingEvery(Duration.ofSeconds(1))
+//                .ignoring(NoSuchElementException.class);
+//        WebElement landscaoe = wait1.until(ExpectedConditions.presenceOfElementLocated(By.id("#myModalLabel")));
+        String landscaoe = addToCartProductInShoppingCart.getText();
+        return landscaoe;
     }
 }
--- src/test/java/lt/techin/exam/ricardas/cizinauskas/PrestaShopTest.java
+++ src/test/java/lt/techin/exam/ricardas/cizinauskas/PrestaShopTest.java
@@ -1,22 +1,62 @@
 package lt.techin.exam.ricardas.cizinauskas;
 
+import jdk.jfr.Description;
 import lt.techin.exam.ricardas.cizinausas.PrestaShopPage;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
 
 public class PrestaShopTest extends BaseTest{
-    protected String productName1;
-    protected String productName2;
 
     @Test
-    void serchForAProduckt(){
+    @Description("Checking product one is product in product list")
+    void getFirstProduct(){
         PrestaShopPage prestaShop = new PrestaShopPage(driver);
         String productName = "Hummingbird printed t-shirt";
         String productName1 = productName.toLowerCase();
-        String productFromEshop = prestaShop.getProductName().toLowerCase();
+        String productFromEshop = prestaShop.getProductNameOne().toLowerCase();
         Assertions.assertEquals(productName1, productFromEshop,"Product name must match product from e-shop name");
-//        System.out.println(prestProduct1);
+    }
+
+    @Test
+    @Description("Checking product two is product in product list")
+    void getSecondProductName(){
+        PrestaShopPage prestaShop = new PrestaShopPage(driver);
+        String productName = "HUMMINGBIRD PRINTED SWEATER";
+        String productName2 = productName.toLowerCase();
+        String productFromEshop = prestaShop.getProductNameTwo().toLowerCase();
+        Assertions.assertEquals(productName2, productFromEshop,"Product name must match product from e-shop name");
+    }
+
+    @Test
+    @Description("Select product add to wishlist(Heaet symbol)")
+    void addToWishlistProductOne() {
+        PrestaShopPage prestaShop = new PrestaShopPage(driver);
+        String webMessage = "You need to be logged in to save products in your wishlist.";
+        String actual = prestaShop.firstItemTowhishlist();
+        Assertions.assertEquals(webMessage,actual,"If you not Signed in following message will appear: You need to be logged in to save products in your wishlist");
+
+    }
+
+    @Test
+    @Description("Second product add to wishlist(Heart symbol)")
+    void addToWhishlistProductTwo() {
+        PrestaShopPage prestaShop = new PrestaShopPage(driver);
+        String webMessage = "You need to be logged in to save products in your wishlist.";
+        String actual = prestaShop.secondItemToWhislist();
+        Assertions.assertEquals(webMessage,actual,"If you not Signed in following message will appear: You need to be logged in to save products in your wishlist");
+    }
+    @Test
+    @Description("First product.Press cencel and select size and click add to cart")
+    void addToCartProductOne() throws InterruptedException {
+        PrestaShopPage prstaShop = new PrestaShopPage(driver);
+        prstaShop.firstItemTowhishlist();
+        prstaShop.firstItemClickCansel();
+        prstaShop.selectItemSize();
+        Thread.sleep(5000);
+        String webElement = String.valueOf(prstaShop.selectProductItemMessageAddedToCart()).substring(1).trim();
+        Assertions.assertEquals("Product successfully added to your shopping cart",webElement);
     }
 }
-
